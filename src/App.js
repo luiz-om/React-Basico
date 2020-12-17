3a613f5aa9ff4f0309ceb75ea3a9895fa78716ef
Luiz Fernando de Oliveira Martins <luizfernando_martins@hotmail.com>
2020-12-17 19:36:10 -0400

    criação de componentes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Comentario from './Components/Comentario'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Meu primeiro projeto</h1>
+
+      <Comentario nome='Luiz Fernando' email='teste@teste.com' data={new Date(2020, 2,2)}>
+  ola mensagem
+</Comentario>
+<Comentario nome='Fernando' email='teste3@teste.com' data={new Date(2020, 3,2)}>
+  ola mensagem segunda
+</Comentario>
+
     </div>
   );
 }
---
+++ src/Components/Comentario.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Comentario.css'
+
+const Comentario= props =>(
+    <div className="Comentario">
+        <h2>{props.nome}</h2>
+        <p>{props.email}</p>
+         <p>{props.children}</p>
+         <p>{props.data.toString()}</p>
+    </div>
+)
+
+
+export default Comentario
